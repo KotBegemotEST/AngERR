151e6a253d6fa0e627632b0914b44890a3a3c444
KotBegemotEST <buketov.95@mail.ru>
2025-06-29 17:30:34 +0300

    updated styles

--- src/app/horizontal-strip/horizontal-strip.ts
+++ src/app/horizontal-strip/horizontal-strip.ts
@@ -1,4 +1,12 @@
-import { Component, Input,OnChanges,OnInit  } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  HostListener
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
@@ -9,16 +17,53 @@ import { CommonModule } from '@angular/common';
   templateUrl: './horizontal-strip.html',
   styleUrl: './horizontal-strip.css'
 })
-export class HorizontalStripComponent implements OnInit {
+
+export class HorizontalStripComponent implements OnInit, AfterViewInit {
   @Input() block: any;
   loading = true;
 
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
+
+  showLeft = false;
+  showRight = true;
+
   ngOnInit(): void {
-    // задержка, чтобы успел показаться loader
     setTimeout(() => {
       if (this.block?.data?.length > 0) {
         this.loading = false;
       }
-    }, 100); // 100 мс — визуально даст шанс показать крутилку
+    }, 100);
+  }
+
+  ngAfterViewInit(): void {
+    this.updateButtons();
+  }
+
+  scrollLeft(): void {
+    const container = this.scrollContainer.nativeElement;
+    container.scrollBy({ left: -container.clientWidth, behavior: 'smooth' });
+
+    setTimeout(() => this.updateButtons(), 300);
+  }
+
+  scrollRight(): void {
+    const container = this.scrollContainer.nativeElement;
+    container.scrollBy({ left: container.clientWidth, behavior: 'smooth' });
+
+    setTimeout(() => this.updateButtons(), 300);
+  }
+
+  updateButtons(): void {
+    const container = this.scrollContainer.nativeElement;
+    const scrollLeft = container.scrollLeft;
+    const maxScrollLeft = container.scrollWidth - container.clientWidth;
+
+    this.showLeft = scrollLeft > 0;
+    this.showRight = scrollLeft < maxScrollLeft - 10;
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    this.updateButtons();
   }
 }
